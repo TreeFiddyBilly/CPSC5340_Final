b469669e50bebfd47456f937d9bbec04890d1633
Logan Graham <lhg0014"auburn.edu>
2025-05-31 14:57:10 +0100

    Final tweaks for Airline Detail. Nothing crazy just better alignment.

--- Assignment3/Views/AirlineDetail.swift
+++ Assignment3/Views/AirlineDetail.swift
@@ -12,19 +12,25 @@ struct AirlineDetail: View {
     var airline : AirlineModel
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text(airline.airline_name)
-            Text("Airline ID: ") + Text(airline.airline_id)
-            Text("Average Fleet Age: ") + Text(airline.fleet_average_age)
-            Text("Call Sign: ") + Text(airline.callsign)
-            Text("Hub Code: ") + Text(airline.hub_code ?? "N/A")
-            Text("Country: ") + Text(airline.country_name)
-            Text("Date Founded: ") + Text(airline.date_founded ?? "N/A")
-            Text("Fleet Size: ") + Text(airline.fleet_size)
-            Text("Status: ") + Text(airline.status)
+        VStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(airline.airline_name)
+                Text("Airline ID: ") + Text(airline.airline_id)
+                Text("Average Fleet Age: ") + Text(airline.fleet_average_age)
+                Text("Call Sign: ") + Text(airline.callsign)
+                Text("Hub Code: ") + Text(airline.hub_code ?? "N/A")
+                Text("Country: ") + Text(airline.country_name)
+                Text("Date Founded: ") + Text(airline.date_founded ?? "N/A")
+                Text("Fleet Size: ") + Text(airline.fleet_size)
+                Text("Status: ") + Text(airline.status)
+                
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
             
+            Spacer()
         }
-        
+        .frame(maxHeight: .infinity, alignment: .top)
     }
 }
 
